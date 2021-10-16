370088975ade5e7e4ab8b1817e32578668e0b30e
arnavmahajan-33 <arnavmahajan33@gmail.com>
2021-10-16 13:22:41 +0530

    first upploadind by git bash

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+var player;
+var score=0;
+var wormGroup;
+
+function preload(){
+  wormImage = loadImage("images/snake.png");
+  bgImage = loadImage("images/bgImg.png");
+  playerImage = loadImage("images/bunny.png");
+  }
+
+function setup() {
+createCanvas(600,600);
+
+bg=createSprite(300,300);
+bg.scale=4;
+bg.addImage(bgImage);
+
+player = createSprite(40,550,30,30);
+player.addImage(playerImage);
+player.scale=0.4;
+wormGroup= new Group();
+}
+
+function draw() {
+background("black");  
+stroke("red")
+noFill();
+ellipse(100,350,40,30);
+player.x= mouseX;
+player.y= mouseY;
+
+if(player.x<150 && player.x>90 && player.y<380 && player.y>320 ){
+  text("NO CHEATING!!",200,200);
+  player.x=30;
+  player.y=30;
+}
+generateWorms();
+
+for(var i = 0 ; i< (wormGroup).length ;i++){
+  var temp = (wormGroup).get(i) ;
+  if (player.isTouching(temp)) {
+    score++;
+    temp.destroy();
+    temp=null;
+    }   
+  }
+
+  drawSprites();
+  textSize(20);
+  text("Worms destroyed: "+score,350,50);
+}
+
+function generateWorms(){
+if(frameCount % 20===0){
+  
+  var worm=createSprite(random(40,380),random(290,380),40,5);
+  worm.scale=random(0.1,0.3);
+  worm.addImage(wormImage);
+  worm.shapeColor="green";
+  worm.velocityX=random(-4,4);
+  worm.velocityY=random(-4,4);
+  wormGroup.add(worm);
+  }
+}
